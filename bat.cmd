017a81b69b66f0f03ddb6fcd62f0e980ad02e249
Feldwor <BoQsc@users.noreply.github.com>
2026-04-26 16:39:33 +0200

    Add files via upload

---
+++ bat.cmd
@@ -0,0 +1,5 @@
+
+litert-lm-single-windows-x64.exe ^
+  --backend=cpu ^
+  --model_path="C:\Users\Windows10_new\Downloads\gemma-4-E2B-it.litertlm" ^
+  --input_prompt="Hello. Write one short paragraph."
